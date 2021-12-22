2b7547d9891de9125352d60dda5f97e093f082fb
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2021-12-22 21:31:53 +0100

    Update mem.cpp

--- UniversalDllBase/mem.cpp
+++ UniversalDllBase/mem.cpp
@@ -1,28 +1,52 @@
+#pragma warning(disable : 6387)
+
 #include "mem.h"
+#include "hack.h"
+
 #include <psapi.h>
 
 uintptr_t mem::resolvePointer(std::vector<uintptr_t> offsets, uintptr_t base) {
-    uintptr_t ptr = base;
-    for (uintptr_t offset : offsets) {
-        ptr = *(uintptr_t*)ptr + offset;
-    }
-    return ptr;
+	uintptr_t ptr = base;
+	for (uintptr_t offset : offsets) {
+		ptr = *(uintptr_t*)ptr + offset;
+	}
+	return ptr;
 }
 
-uintptr_t mem::patternScan(const char* module, const char* signature) {
-    MODULEINFO modInfo;
-    GetModuleInformation(GetCurrentProcess(), GetModuleHandleA(module), &modInfo, sizeof(MODULEINFO));
-    uintptr_t start = (uintptr_t)modInfo.lpBaseOfDll;
-    uintptr_t end = start + modInfo.SizeOfImage;
-    const char* pat = signature;
-    const char* pat_end = pat + strlen(signature);
-    for (uintptr_t pCur = start; pCur < end; pCur++) {
-        if (!*pat)
-            return pCur;
-        if (*(BYTE*)pCur == *(BYTE*)pat)
-            pat = pat_end;
-        else
-            pat = signature;
-    }
-    return 0;
+#define InRange(x, a, b) (x >= a && x <= b) 
+#define GetBits(x) (InRange((x & (~0x20)), 'A', 'F') ? ((x & (~0x20)) - 'A' + 0xA): (InRange(x, '0', '9') ? x - '0': 0))
+#define GetByte(x) (GetBits(x[0]) << 4 | GetBits(x[1]))
+
+uintptr_t mem::FindPatternEx(const uintptr_t& pStartAddr, const uintptr_t& pEndAddr, const char* szTPattern) {
+	const char* szPattern = szTPattern;
+	uintptr_t pFirstMatch = NULL;
+
+	for (uintptr_t pPosition = pStartAddr; pPosition < pEndAddr; pPosition++) {
+		if (!*szPattern) return pFirstMatch;
+		const uint8_t pCurrentPattern = *reinterpret_cast<const uint8_t*>(szPattern);
+		const uint8_t pCurrentMemory = *reinterpret_cast<const uint8_t*>(pPosition);
+
+		if (pCurrentPattern == '\?' || pCurrentMemory == GetByte(szPattern)) {
+			if (!pFirstMatch) pFirstMatch = pPosition;
+			if (!szPattern[2]) return pFirstMatch;
+			szPattern += pCurrentPattern != '\?' ? 3 : 2;
+		}
+		else {
+			szPattern = szTPattern;
+			pFirstMatch = NULL;
+		}
+	}
+	return NULL;
+}
+
+uintptr_t mem::FindPattern(const char* szModule, const char* szPattern) {
+	MODULEINFO hModInfo = { nullptr };
+	if (!K32GetModuleInformation(GetCurrentProcess(), GetModuleHandleA(szModule), &hModInfo, sizeof(hModInfo))) return NULL;
+	const uintptr_t pStartAddr = (uintptr_t)hModInfo.lpBaseOfDll;
+	const uintptr_t pEndAddr = pStartAddr + hModInfo.SizeOfImage;
+	return FindPatternEx(pStartAddr, pEndAddr, szPattern);
+}
+
+HWND mem::GetWindowHandle(const char* windowName) {
+	return FindWindowA(NULL, windowName);
 }
