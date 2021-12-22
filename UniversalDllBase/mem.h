df44427cc291ad57a0497ae560df8b5c466b5dd2
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2021-12-22 21:32:14 +0100

    Improved the pattern scanner

--- UniversalDllBase/mem.h
+++ UniversalDllBase/mem.h
@@ -4,5 +4,7 @@
 
 namespace mem {
     uintptr_t resolvePointer(std::vector<uintptr_t> offsets, uintptr_t base);
-    uintptr_t patternScan(const char* module, const char* signature);
+    uintptr_t FindPatternEx(const uintptr_t& pStartAddr, const uintptr_t& pEndAddr, const char* szTPattern);
+    uintptr_t FindPattern(const char* szModule, const char* szPattern);
+    HWND GetWindowHandle(const char* windowName);
 }
